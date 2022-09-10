7e652e2a4e1e11bf392e75bd77f25c1a1ba93574
Lachezar Kutsarov <lkutsarov@gmail.com>
2022-09-10 13:43:51 +0200

    Create Solution.cpp

---
+++ Solution.cpp
@@ -0,0 +1,30 @@
+
+#include <queue>
+#include <vector>
+using namespace std;
+
+class Solution {
+    
+public:
+    int connectSticks(vector<int>& sticks) {
+        priority_queue<int, vector<int>, greater<>> minHeap;
+        for (const auto& stick : sticks) {
+            minHeap.push(stick);
+        }
+
+        int costToConnectAllSticks = 0;
+        while (minHeap.size() > 1) {
+            int connectedStick = removeAndReturnTop(minHeap) + removeAndReturnTop(minHeap);
+            costToConnectAllSticks += connectedStick;
+            minHeap.push(connectedStick);
+        }
+        return costToConnectAllSticks;
+    }
+
+private:
+    int removeAndReturnTop(priority_queue<int, vector<int>, greater<>>&minHeap) {
+        int top = minHeap.top();
+        minHeap.pop();
+        return top;
+    }
+};
